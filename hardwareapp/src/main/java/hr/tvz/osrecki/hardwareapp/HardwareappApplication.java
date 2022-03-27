25c3c999f0a085b3c3d34da1b4b3440ce09adfc7
Matija Osrečki <matija.osrecki@gmail.com>
2022-03-27 13:00:52 +0200

    Initial commit, setup first Spring project

---
+++ hardwareapp/src/main/java/hr/tvz/osrecki/hardwareapp/HardwareappApplication.java
@@ -0,0 +1,12 @@
+package hr.tvz.osrecki.hardwareapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HardwareappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HardwareappApplication.class, args);
+    }
+}
