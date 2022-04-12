2044f0980bcc1ca5f70cdc9bcc2f990fc330da09
Matija Osrečki <matija.osrecki@gmail.com>
2022-04-12 18:18:59 +0200

    Add more mocked hardware

--- hardwareapp-frontend/src/stores/hardware.ts
+++ hardwareapp-frontend/src/stores/hardware.ts
@@ -13,6 +13,26 @@ export const useHardwareStore = defineStore("hardware", () => {
       price: 614.99,
     },
     {
+      code: "N82E16819113683",
+      name: "AMD Ryzen 5 5600GE",
+      price: 220,
+    },
+    {
+      code: "N82E16819113664",
+      name: "AMD Ryzen 9 5900X ",
+      price: 395,
+    },
+    {
+      code: "N82E16819113665",
+      name: "AMD Ryzen 7 5800X",
+      price: 359,
+    },
+    {
+      code: "N82E16814932443",
+      name: "GIGABYTE Gaming GeForce RTX 3070 Ti",
+      price: 858.33,
+    },
+    {
       code: "MZV7S500BAM",
       name: "SAMSUNG 970 EVO PLUS",
       price: 69.99,
@@ -23,14 +43,34 @@ export const useHardwareStore = defineStore("hardware", () => {
       price: 149.99,
     },
     {
+      code: "BLM2K8G44C19U4B",
+      name: "Crucial Ballistix MAX 4400 MHz",
+      price: 79.99,
+    },
+    {
+      code: "N82E16820232880",
+      name: "G.SKILL Ripjaws V Series 16GB DDR4",
+      price: 85.99,
+    },
+    {
+      code: "N82E16820821488",
+      name: "OLOy Blade RGB 32GB DDR5",
+      price: 309.99,
+    },
+    {
       code: "CMW32GX4M2D3600C18",
       name: "CORSAIR Vengeance RGB Pro 32GB",
       price: 124.99,
     },
     {
-      code: "N82E16814932443",
-      name: "GIGABYTE Gaming GeForce RTX 3070 Ti",
-      price: 858.33,
+      code: "N82E16813119500",
+      name: "Asus ROG STRIX B550-F",
+      price: 229.99,
+    },
+    {
+      code: "N82E16813119504",
+      name: "ASUS ROG Maximus Z690 Hero",
+      price: 599.99,
     },
   ];
 
--- hardwareapp/src/main/java/hr/tvz/osrecki/hardwareapp/repository/MockHardwareRepository.java
+++ hardwareapp/src/main/java/hr/tvz/osrecki/hardwareapp/repository/MockHardwareRepository.java
@@ -11,11 +11,20 @@ public class MockHardwareRepository implements HardwareRepository {
     private final Set<Hardware> MOCK_HARDWARE = new HashSet<>(Arrays.asList(
             new Hardware("Intel Core i7-12700K", "BX8071512700K", 384.98, HardwareType.CPU, 14),
             new Hardware("Intel Core i9-12900K", "BX8071512900K", 614.99, HardwareType.CPU, 6),
+            new Hardware("AMD Ryzen 5 5600GE", "N82E16819113683", 220.00, HardwareType.CPU, 22),
+            new Hardware("AMD Ryzen 9 5900X", "N82E16819113664", 395.00, HardwareType.CPU, 11),
+            new Hardware("AMD Ryzen 7 5800X", "N82E16819113665", 359.00, HardwareType.CPU, 28),
+            new Hardware("GIGABYTE Gaming GeForce RTX 3070 Ti", "N82E16814932443", 858.33, HardwareType.GPU, 3),
             new Hardware("SAMSUNG 970 EVO PLUS", "MZV7S500BAM", 69.99, HardwareType.STORAGE, 31),
             new Hardware("Western Digital WD BLACK SN850", "WDS100T1X0E", 149.99, HardwareType.STORAGE, 18),
+            new Hardware("Crucial Ballistix MAX 4400 MHz", "BLM2K8G44C19U4B", 79.99, HardwareType.STORAGE, 20),
+            new Hardware("G.SKILL Ripjaws V Series 16GB DDR4", "N82E16820232880", 85.99, HardwareType.RAM, 16),
+            new Hardware("OLOy Blade RGB 32GB DDR5", "N82E16820821488", 309.99, HardwareType.RAM, 3),
             new Hardware("CORSAIR Vengeance RGB Pro 32GB", "CMW32GX4M2D3600C18", 124.99, HardwareType.RAM, 9),
-            new Hardware("GIGABYTE Gaming GeForce RTX 3070 Ti", "N82E16814932443", 858.33, HardwareType.GPU, 3)
+            new Hardware("Asus ROG STRIX B550-F", "N82E16813119500", 229.99, HardwareType.MBO, 1),
+            new Hardware("ASUS ROG Maximus Z690 Hero", "N82E16813119504", 599.99, HardwareType.MBO, 5)
     ));
+
 
     @Override
     public Set<Hardware> findAll() {
