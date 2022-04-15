4baeb0982d728e9f7bd94a32f6a98a08908f5984
Matija Osrečki <matija.osrecki@gmail.com>
2022-04-15 23:53:43 +0200

    Add refresh button

--- hardwareapp-frontend/src/views/Hardware/HardwareView.vue
+++ hardwareapp-frontend/src/views/Hardware/HardwareView.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import { useHardwareStore } from "@/stores/hardware";
-import { PhShuffle } from "phosphor-vue";
+import { PhShuffle, PhArrowsClockwise } from "phosphor-vue";
 const store = useHardwareStore();
 
 store.fetchHardware();
@@ -10,11 +10,18 @@ store.fetchHardware();
   <div flex="1 ~ col" h="full">
     <NH1 prefix="bar" type="success" mb="8" flex="~ gap-4" items="center">
       <NText>Available hardware</NText>
-      <NButton quaternary circle @click="store.shuffleHardware">
-        <template #icon>
-          <PhShuffle />
-        </template>
-      </NButton>
+      <div flex="~ gap-1" items="center">
+        <NButton secondary type="primary" circle @click="store.fetchHardware">
+          <template #icon>
+            <PhArrowsClockwise />
+          </template>
+        </NButton>
+        <NButton secondary type="info" circle @click="store.shuffleHardware">
+          <template #icon>
+            <PhShuffle />
+          </template>
+        </NButton>
+      </div>
     </NH1>
     <div flex="1 ~ col-reverse lg:row gap-2 lg:gap-12">
       <HardwareList w="full" flex="1" max-w="lg:100" :items="store.hardware" />
