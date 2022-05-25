8141c0b07a222ce3a2f511d876c9b5f5815be44c
Matija Osrečki <matija.osrecki@gmail.com>
2022-05-25 16:59:54 +0200

    add translations

--- hardwareapp-frontend-angular/src/app/app.component.ts
+++ hardwareapp-frontend-angular/src/app/app.component.ts
@@ -1,24 +1,27 @@
-import {Component} from '@angular/core';
-import {AuthenticationService} from "./security/authentication.service";
-import {Router} from "@angular/router";
+import { Component } from '@angular/core';
+import { AuthenticationService } from './security/authentication.service';
+import { Router } from '@angular/router';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'Hardware App';
 
   constructor(
     public authenticationService: AuthenticationService,
-    private router: Router
+    private router: Router,
+    private translate: TranslateService
   ) {
+    translate.setDefaultLang('en');
+    translate.use('en');
   }
 
   logout() {
-    this.authenticationService.logout();
-    this.router.navigate(['/login']).then();
+    this.authenticationService!.logout();
+    this.router!.navigate(['/login']).then();
   }
-
 }
--- hardwareapp-frontend-angular/src/app/app.module.ts
+++ hardwareapp-frontend-angular/src/app/app.module.ts
@@ -1,18 +1,25 @@
-import {NgModule} from '@angular/core';
-import {BrowserModule} from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
-import {AppRoutingModule} from './app-routing.module';
-import {AppComponent} from './app.component';
-import {HardwaresComponent} from './hardwares/hardwares.component';
-import {HardwareDetailComponent} from './hardware-detail/hardware-detail.component';
-import {HTTP_INTERCEPTORS, HttpClientModule} from "@angular/common/http";
-import {LoginComponent} from './login/login.component';
-import {FormsModule} from "@angular/forms";
-import {HomeComponent} from './home/home.component';
-import {AuthenticationInterceptor} from "./security/authentication.interceptor";
-import {ForbiddenPageComponent} from "./forbidden-page/forbidden-page.component";
-import {PageNotFoundComponent} from "./page-not-found/page-not-found.component";
-import {HardwareNewComponent} from "./hardware-new/hardware-new.component";
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HardwaresComponent } from './hardwares/hardwares.component';
+import { HardwareDetailComponent } from './hardware-detail/hardware-detail.component';
+import {
+  HTTP_INTERCEPTORS,
+  HttpClientModule,
+  HttpClient,
+} from '@angular/common/http';
+import { LoginComponent } from './login/login.component';
+import { FormsModule } from '@angular/forms';
+import { HomeComponent } from './home/home.component';
+import { AuthenticationInterceptor } from './security/authentication.interceptor';
+import { ForbiddenPageComponent } from './forbidden-page/forbidden-page.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { HardwareNewComponent } from './hardware-new/hardware-new.component';
+
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 
 @NgModule({
   declarations: [
@@ -23,22 +30,34 @@ import {HardwareNewComponent} from "./hardware-new/hardware-new.component";
     LoginComponent,
     HomeComponent,
     ForbiddenPageComponent,
-    PageNotFoundComponent
+    PageNotFoundComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient],
+      },
+      defaultLanguage: 'hr',
+    }),
   ],
   providers: [
     {
       provide: HTTP_INTERCEPTORS,
       useClass: AuthenticationInterceptor,
-      multi: true
+      multi: true,
     },
   ],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule {
+export class AppModule {}
+
+// required for AOT compilation
+export function HttpLoaderFactory(http: HttpClient): TranslateHttpLoader {
+  return new TranslateHttpLoader(http);
 }
