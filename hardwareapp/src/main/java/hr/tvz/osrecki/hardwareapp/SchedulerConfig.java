036ad1e3742da2a53e0099b06f9d27c1d8c513e7
Matija Osrečki <matija.osrecki@gmail.com>
2022-06-12 16:42:51 +0200

    Finish lab10

--- hardwareapp/src/main/java/hr/tvz/osrecki/hardwareapp/SchedulerConfig.java
+++ hardwareapp/src/main/java/hr/tvz/osrecki/hardwareapp/SchedulerConfig.java
@@ -4,11 +4,21 @@ import org.quartz.*;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import static org.quartz.CronScheduleBuilder.cronSchedule;
+
 @Configuration
 public class SchedulerConfig {
     @Bean
     public JobDetail objavaAvailableHardware() {
         return JobBuilder.newJob(AvailableHardwareJob.class).withIdentity("availableHardwareTrigger").storeDurably().build();
+    }
+
+    @Bean
+    public CronTrigger hardwareJobTrigger() {
+        return TriggerBuilder.newTrigger().forJob(objavaAvailableHardware())
+                .withIdentity("hardwareTriggerLiveExercise")
+                .withSchedule(cronSchedule("0 0 0 ? * SAT,SUN"))
+                .build();
     }
 
     @Bean
